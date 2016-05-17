f3d8507aa7ab74e0ac52c9b17ea492014b7b50f6
Lou Montulli <lou@montulli.org>
2016-05-17 09:47:48 -0700

    Use appname as issuer use whole email for username

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -2,23 +2,13 @@ module DeviseGoogleAuthenticator
   module Controllers # :nodoc:
     module Helpers # :nodoc:
       def google_authenticator_qrcode(user, qualifier=nil, issuer=nil)
-        username = username_from_email(user.email)
         app = user.class.ga_appname || Rails.application.class.parent_name
-        data = "otpauth://totp/#{otpauth_user(username, app, qualifier)}?secret=#{user.gauth_secret}"
-        data << "&issuer=#{issuer}" if !issuer.nil?
+        data = "otpauth://totp/#{user.email)}?secret=#{user.gauth_secret}"
+        data << "&issuer=#{app}"
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
         return image_tag(url, :alt => 'Google Authenticator QRCode')
       end
-
-      def otpauth_user(username, app, qualifier=nil)
-        "#{username}@#{app}#{qualifier}"
-      end
-
-      def username_from_email(email)
-        (/^(.*)@/).match(email)[1]
-      end
-
     end
   end
 end
