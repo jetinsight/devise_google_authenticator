ffb04763b52bd5aa9ae3b2cba15a4f2ba426d693
Lou Montulli <lou@montulli.org>
2016-05-17 09:55:51 -0700

    fix typo

--- lib/devise_google_authenticatable/controllers/helpers.rb
+++ lib/devise_google_authenticatable/controllers/helpers.rb
@@ -3,7 +3,7 @@ module DeviseGoogleAuthenticator
     module Helpers # :nodoc:
       def google_authenticator_qrcode(user, qualifier=nil, issuer=nil)
         app = user.class.ga_appname || Rails.application.class.parent_name
-        data = "otpauth://totp/#{user.email)}?secret=#{user.gauth_secret}"
+        data = "otpauth://totp/#{user.email}?secret=#{user.gauth_secret}"
         data << "&issuer=#{app}"
         data = Rack::Utils.escape(data)
         url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
